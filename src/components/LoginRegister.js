47e3b007295d7df9fc326c0103510f699b20e958
Shivam Chahar <shivam.chahar@gmail.com>
2021-06-06 15:30:45 +0530

    animation added in login register page

--- src/components/LoginRegister.js
+++ src/components/LoginRegister.js
@@ -21,6 +21,20 @@ class LoginRegister extends React.Component {
 
     }
 
+    componentDidMount() {
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+
+        signUpButton.addEventListener('click', () => {
+            container.classList.add('right-panel-active');
+        });
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove('right-panel-active');
+        });
+    }
+
     submit = async (e) => {
         e.preventDefault();
         const fd = new FormData();
