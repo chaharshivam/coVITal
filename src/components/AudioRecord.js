a9fe4c1dc7fa7ff394ddb4496a071a32c4857b2b
Shivam Chahar <shivam.chahar@gmail.com>
2021-06-09 16:13:38 +0530

    toast messages added in AudioRecord.js

--- src/components/AudioRecord.js
+++ src/components/AudioRecord.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ReactMic } from 'react-mic';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./css/AudioRecord.css"
+import axios from "axios";
 
 export class AudioRecord extends React.Component {
     constructor(props) {
@@ -13,11 +16,31 @@ export class AudioRecord extends React.Component {
 
     startRecording = () => {
         this.setState({ record: true, button:"btn btn-primary mybutton" });
+        toast.info('Audio Recording Started', {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     }
 
     stopRecording = () => {
-        this.setState({ record: false, button:"btn btn-primary mybutton disabled"  });
+        this.setState({ record: false, button:"btn btn-primary mybutton disabled" });
+        toast.error('Audio Recording Stopped', {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+
     }
+
 
     onData(recordedBlob) {
         console.log('chunk of real-time data is: ', recordedBlob);
@@ -26,6 +49,7 @@ export class AudioRecord extends React.Component {
     onStop(recordedBlob) {
         console.log('recordedBlob is: ', recordedBlob);
     }
+
 
     render() {
         return (
@@ -43,15 +67,28 @@ export class AudioRecord extends React.Component {
                                 onStop={this.onStop}
                                 onData={this.onData}
                                 strokeColor="#000000"
-                                backgroundColor="#fff" />
+                                backgroundColor="#fff"
+                                mimeType="audio/mp3"
+                            />
                         </p>
                         <button className="btn btn-primary mybutton" onClick={this.startRecording} type="button">Start</button>
-                        <button className={`${this.state.button}`} onClick={this.stopRecording} type="button">Stop</button>
+                        <button className={`${this.state.button}`} onClick={this.stopRecording} type="button">Stop/Upload</button>
                     </div>
                     <div className="card-footer text-muted">
                         Last checked 2 days ago
                     </div>
                 </div>
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
             </div>
         );
     }
